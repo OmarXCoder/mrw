6ef72185eb87435d70d873845f92db87e9cf6157
Omar Ali <omarXcoder@gmail.com>
2022-07-03 02:57:56 +0200

    install and initialize tailwindcss

--- nova-components/ReportPageGenerator/webpack.mix.js
+++ nova-components/ReportPageGenerator/webpack.mix.js
@@ -5,7 +5,8 @@ require('./nova.mix');
 mix.setPublicPath('dist')
     .js('resources/js/tool.js', 'js')
     .vue({ version: 3 })
-    .css('resources/css/tool.css', 'css')
+    // .css('resources/css/tool.css', 'css')
+    .postCss('resources/css/tool.css', 'css', [require('tailwindcss')])
     .nova('mrw/report-page-generator')
     .alias({
         '@': path.join(__dirname, 'resources/js'),
