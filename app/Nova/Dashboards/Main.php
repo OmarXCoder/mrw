b9a9f9ebabb485823d002f940f58b7bf606758ad
Omar Ali <omarxcoder@gmail.com>
2022-07-13 19:48:00 +0200

    display relevant cards for the logged in user in the main dashboard - resolves #57

--- app/Nova/Dashboards/Main.php
+++ app/Nova/Dashboards/Main.php
@@ -8,7 +8,7 @@ use App\Nova\Metrics\TotalEvents;
 use App\Nova\Metrics\InteractionsByDays;
 use App\Nova\Metrics\TotalShows;
 use App\Nova\Metrics\UsersPerType;
-use Laravel\Nova\Cards\Help;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Nova\Dashboards\Main as Dashboard;
 
 class Main extends Dashboard
@@ -20,14 +20,24 @@ class Main extends Dashboard
      */
     public function cards()
     {
-        return [
-            // new Help,
+        return Auth::user()->isClientTeamMember() ? $this->clientCards() : [
             TotalClients::make()->width('1/3')->defaultRange('ALL'),
             TotalShows::make()->width('1/3')->defaultRange('ALL'),
             TotalApps::make()->width('1/3')->defaultRange('ALL'),
             TotalAttendees::make()->width('1/4')->defaultRange('ALL'),
             TotalEvents::make()->width('1/4')->defaultRange('ALL'),
             UsersPerType::make()->width('1/2'),
+            InteractionsByDays::make()->width('full'),
+        ];
+    }
+
+    public function clientCards(): array
+    {
+        return [
+            TotalShows::make()->width('1/4')->defaultRange('ALL'),
+            TotalApps::make()->width('1/4')->defaultRange('ALL'),
+            TotalAttendees::make()->width('1/4')->defaultRange('ALL'),
+            TotalEvents::make()->width('1/4')->defaultRange('ALL'),
             InteractionsByDays::make()->width('full'),
         ];
     }
