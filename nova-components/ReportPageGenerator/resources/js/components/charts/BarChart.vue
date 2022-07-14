a25953f89bd852d027e9868227148503879bc51d
Omar Ali <omarxcoder@gmail.com>
2022-07-14 18:00:21 +0200

    show chart data labels

--- nova-components/ReportPageGenerator/resources/js/components/charts/BarChart.vue
+++ nova-components/ReportPageGenerator/resources/js/components/charts/BarChart.vue
@@ -1,6 +1,6 @@
 <template>
     <Bar
-        :chart-options="options"
+        :chart-options="chartOptions"
         :chart-data="data"
         :chart-id="chartId"
         :dataset-id-key="datasetIdKey"
@@ -13,6 +13,7 @@
 </template>
 
 <script setup>
+import { merge } from 'lodash';
 import { Bar } from 'vue-chartjs';
 import chartProps from '@/mixins/chart-props';
 
@@ -30,5 +31,18 @@ ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 const props = defineProps({
     ...chartProps,
+});
+
+const chartOptions = merge(props.options, {
+    plugins: {
+        datalabels: {
+            color: '#64748b',
+            font: {
+                weight: 'thin',
+            },
+            align: 'end',
+            anchor: 'end',
+        },
+    },
 });
 </script>
